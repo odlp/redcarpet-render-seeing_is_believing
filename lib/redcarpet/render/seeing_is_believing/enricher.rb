2af6494c8ee07b6e597aeae986f20db29f679029
Oli Peate <oliverp@gmail.com>
2018-01-07 19:48:24 +0000

    Extract private Enricher methods

--- lib/redcarpet/render/seeing_is_believing/enricher.rb
+++ lib/redcarpet/render/seeing_is_believing/enricher.rb
@@ -5,18 +5,26 @@ module Redcarpet
     module SeeingIsBelieving
       class Enricher
         def process(code)
-          evaluated_code = ::SeeingIsBelieving.call(code).result
-
           code.split("\n").
-            zip(evaluated_code).
-            map do |code_line, eval_lines|
-              if eval_lines.any?
-                code_line + " # => " + eval_lines.join("\n")
-              else
-                code_line
-              end
-            end.
+            zip(evaluate_code(code)).
+            map(&combine_code_and_eval_lines).
             join("\n")
+        end
+
+        private
+
+        def evaluate_code(code)
+          ::SeeingIsBelieving.call(code).result
+        end
+
+        def combine_code_and_eval_lines
+          proc do |code_line, eval_lines|
+            if eval_lines.any?
+              code_line + " # => " + eval_lines.join("\n")
+            else
+              code_line
+            end
+          end
         end
       end
     end
